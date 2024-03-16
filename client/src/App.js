29b6c45d24b89bfed475719ef80b4ccebf72f219
Om <ompol1705@gmail.com>
2024-03-16 22:43:04 +0530

    Graph added

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,37 @@
-import './App.css';
+import { useState } from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate
 } from "react-router-dom";
+import ThemeContext from "./context/ThemeContext";
+import StockContext from "./context/StockContext";
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   const user = localStorage.getItem("token");
+  const [darkMode, setDarkMode] = useState(false);
+  const [stockSymbol, setStockSymbol] = useState("FB");
+
   return (
     <>
-      <Router>
-        <Routes>
-          {user && <Route path="/" exact element={<Home />} />}
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/" element={<Navigate replace to="/login" />} />
-        </Routes>
-      </Router>
+      <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+        <StockContext.Provider value={{ stockSymbol, setStockSymbol }}>
+          <Router>
+            <Routes>
+                {user && <Route path="/" exact element={<Home />} />}
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/" element={<Navigate replace to="/login" />} />
+            </Routes>
+          </Router>
+        </StockContext.Provider>
+      </ThemeContext.Provider>
     </>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,22 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.{html,js,jsx}", 'node_modules/flowbite-react/lib/esm/**/*.js'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "white",
+        none: "none",
+      },
+      borderWidth: {
+        1: "1px",
+      },
+      fontFamily: {
+        quicksand: ["Quicksand", "sans-serif"],
+      },
+      gridTemplateRows: {
+        7: "repeat(7, minmax(0, 1fr))",
+        8: "repeat(8, minmax(0, 1fr))",
+      },
+    },
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
-}
-
+  plugins: [],
+};
